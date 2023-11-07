66fe94fb3fd5f95dd7f0e13675fd2fef4a687fa5
sidn77 <siddhantn77@gmail.com>
2023-11-07 17:24:01 -0500

    Increase code modularity

--- pinecone_index.py
+++ pinecone_index.py
@@ -1,7 +1,5 @@
 import os
 import pinecone
-import time
-import yaml
 
 import pandas as pd
 
@@ -20,6 +18,10 @@ class PinceconeIndex:
     def connect_index(self, embedding_dimension: int,
                       delete_existing: bool = False):
         index_name = self.index_name
+
+        # Check for existence of keys
+        assert os.getenv('PINECONE_KEY') != None
+        assert os.getenv('PINECONE_ENV') != None
 
         pinecone.init(
             api_key=os.getenv('PINECONE_KEY'),
@@ -49,42 +51,3 @@ class PinceconeIndex:
         res = docsearch.similarity_search(query, k=top_k)
 
         return [doc.page_content for doc in res]
-
-
-if __name__ == '__main__':
-    config_path = 'config.yml'
-    with open('config.yml', 'r') as file:
-        config = yaml.safe_load(file)
-
-    print(config)
-
-    data_path = config['paths']['data_path']
-    project = config['paths']['project']
-    format = '.csv'
-
-    index_name = config['pinecone']['index-name']
-    embedding_model = config['sentence-transformers'][
-        'model-name']
-    embedding_dimension = config['sentence-transformers'][
-        'embedding-dimension']
-    delete_existing = True
-
-    file_path_embedding = data_path+project+format
-    df = pd.read_csv(file_path_embedding, index_col=0)
-    print(df.head())
-
-    start_time = time.time()
-    index = PinceconeIndex(index_name, embedding_model)
-    index.connect_index(embedding_dimension, delete_existing)
-    index.upsert_docs(df, 'chunks')
-    end_time = time.time()
-    print(f'Indexing took {end_time - start_time} seconds')
-
-    index = PinceconeIndex(index_name, embedding_model)
-    index.connect_index(embedding_dimension, delete_existing=False)
-
-    query = "When was the student code of conduct last revised?"
-    res = index.query(query, top_k=5)
-
-    assert len(res) == 5
-    print(res)
